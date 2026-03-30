b9f3758929a1098c8a798309f1d4417bc0c8411d
toidicakhia <108525966+toidicakhia@users.noreply.github.com>
2026-03-30 18:45:39 +0700

    use zig 0.14.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,7 +101,7 @@ sourceSets {
 }
 
 zig {
-    zigVersion = "0.14.0"
+    zigVersion = "0.14.1"
     outputDir = layout.buildDirectory.dir("zig")
     targets {
         create("x86_64-linux-gnu")
